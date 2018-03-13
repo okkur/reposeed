facec28cb58b20b324d80c07af56d5fe7f1eaf74
Erfan Besharat <erbesharat@gmail.com>
2018-03-13 18:29:06 +0330

    Add JSON support to config struct

--- cmd/reposeed/config/config.go
+++ cmd/reposeed/config/config.go
@@ -2,68 +2,68 @@ package config
 
 type Config struct {
 	Project struct {
-		Name        string `yaml:"name"`
-		Description string `yaml:"description"`
-		State       string `yaml:"state,omitempty"`
-		OneLiner    string `yaml:"oneLiner,omitempty"`
-		Image       string `yaml:"image,omitempty"`
-		Website     string `yaml:"website,omitempty"`
-		Version     string `yaml:"version,omitempty"`
-		MainLicense string `yaml:"mainLicense,omitempty"`
-		DocsLicense string `yaml:"docsLicense,omitempty"`
-	} `yaml:"project"`
+		Name        string `yaml:"name" json:"name"`
+		Description string `yaml:"description" json:"description"`
+		State       string `yaml:"state,omitempty" json:"state,omitempty"`
+		OneLiner    string `yaml:"oneLiner,omitempty" json:"oneLiner,omitempty"`
+		Image       string `yaml:"image,omitempty" json:"image,omitempty"`
+		Website     string `yaml:"website,omitempty" json:"website,omitempty"`
+		Version     string `yaml:"version,omitempty" json:"version,omitempty"`
+		MainLicense string `yaml:"mainLicense,omitempty" json:"mainLicense,omitempty"`
+		DocsLicense string `yaml:"docsLicense,omitempty" json:"docsLicense,omitempty"`
+	} `yaml:"project" json:"project"`
 	Vision struct {
-		Type     string   `yaml:"type"`
-		Items    []string `yaml:"items"`
-		Concept  string   `yaml:"concept"`
-		Overview string   `yaml:"overview"`
-		Aim      string   `yaml:"aim"`
-	} `yaml:"vision"`
+		Type     string   `yaml:"type" json:"type"`
+		Items    []string `yaml:"items" json:"items"`
+		Concept  string   `yaml:"concept" json:"concept"`
+		Overview string   `yaml:"overview" json:"overview"`
+		Aim      string   `yaml:"aim" json:"aim"`
+	} `yaml:"vision" json:"vision"`
 	Repo struct {
-		Type string `yaml:"type"`
-		Link string `yaml:"link"`
-	} `yaml:"repo"`
+		Type string `yaml:"type" json:"type"`
+		Link string `yaml:"link" json:"link"`
+	} `yaml:"repo" json:"repo"`
 	Copyright struct {
-		Owner string `yaml:"owner"`
-		Year  string `yaml:"year"`
-	} `yaml:"copyright"`
+		Owner string `yaml:"owner" json:"owner"`
+		Year  string `yaml:"year" json:"year"`
+	} `yaml:"copyright" json:"copyright"`
 	Cla struct {
-		CopyrightHolder string `yaml:"copyrightHolder"`
-	} `yaml:"cla"`
+		CopyrightHolder string `yaml:"copyrightHolder" json:"copyrightHolder"`
+	} `yaml:"cla" json:"cla"`
 	Maintainers []struct {
-		Name string `yaml:"name"`
-		Nick string `yaml:"nick,omitempty"`
-	} `yaml:"maintainers,omitempty"`
+		Name string `yaml:"name" json:"name"`
+		Nick string `yaml:"nick,omitempty" json:"nick,omitempty"`
+	} `yaml:"maintainers,omitempty" json:"maintainers,omitempty"`
 	Emails struct {
-		CommercialSupport string `yaml:"commercialSupport,omitempty"`
-		Security          string `yaml:"security"`
-		Coc               string `yaml:"coc"`
-	} `yaml:"emails"`
+		CommercialSupport string `yaml:"commercialSupport,omitempty" json:"commercialSupport,omitempty"`
+		Security          string `yaml:"security" json:"security"`
+		Coc               string `yaml:"coc" json:"coc"`
+	} `yaml:"emails" json:"emails"`
 	Badges []struct {
-		Image string `yaml:"image,omitempty"`
-		Link  string `yaml:"link,omitempty"`
-		Alt   string `yaml:"alt,omitempty"`
-	} `yaml:"badges"`
+		Image string `yaml:"image,omitempty" json:"image,omitempty"`
+		Link  string `yaml:"link,omitempty" json:"link,omitempty"`
+		Alt   string `yaml:"alt,omitempty" json:"alt,omitempty"`
+	} `yaml:"badges" json:"badges"`
 	SupportLinks struct {
-		Documentation   string `yaml:"documentation,omitempty"`
-		Examples        string `yaml:"examples,omitempty"`
-		Troubleshooting string `yaml:"troubleshooting,omitempty"`
-	} `yaml:"supportLinks,omitempty"`
+		Documentation   string `yaml:"documentation,omitempty" json:"documentation,omitempty"`
+		Examples        string `yaml:"examples,omitempty" json:"examples,omitempty"`
+		Troubleshooting string `yaml:"troubleshooting,omitempty" json:"troubleshooting,omitempty"`
+	} `yaml:"supportLinks,omitempty" json:"supportLinks,omitempty"`
 	Readme struct {
-		UsageExample string `yaml:"usageExample,omitempty"`
-	} `yaml:"readme,omitempty"`
+		UsageExample string `yaml:"usageExample,omitempty" json:"usageExample,omitempty"`
+	} `yaml:"readme,omitempty" json:"readme,omitempty"`
 	Docs struct {
-		Development string `yaml:"development,omitempty"`
-	} `yaml:"docs,omitempty"`
+		Development string `yaml:"development,omitempty" json:"development,omitempty"`
+	} `yaml:"docs,omitempty" json:"docs,omitempty"`
 	SupportPlatforms []struct {
-		Service string `yaml:"service"`
-		Link    string `yaml:"link"`
-	} `yaml:"supportPlatforms,omitempty"`
+		Service string `yaml:"service" json:"service"`
+		Link    string `yaml:"link" json:"link"`
+	} `yaml:"supportPlatforms,omitempty" json:"supportPlatforms,omitempty"`
 	IssueTemplate struct {
-		Questions []string `yaml:"questions"`
-	} `yaml:"issueTemplate"`
+		Questions []string `yaml:"questions" json:"questions"`
+	} `yaml:"issueTemplate" json:"issueTemplate"`
 	ContributionLinks struct {
-		IssueTemplate string `yaml:"issueTemplate,omitempty"`
-		StarterIssues string `yaml:"starterIssues,omitempty"`
-	} `yaml:"contributionLinks"`
+		IssueTemplate string `yaml:"issueTemplate,omitempty" json:"issueTemplate,omitempty"`
+		StarterIssues string `yaml:"starterIssues,omitempty" json:"starterIssues,omitempty"`
+	} `yaml:"contributionLinks" json:"contributionLinks"`
 }
